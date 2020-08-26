de5c95fa46ed3f2dd534a680823c915f7ee54729
Mehedi <mehedi.cse.002@gmail.com>
2020-08-26 23:22:40 +0600

    Contact function done

--- assets/js/app-jquery.js
+++ assets/js/app-jquery.js
@@ -11,15 +11,15 @@
 
     //scroll
     WebSite.prototype.initNavbarStickey = function () {
-            $(window).scroll(function () {
-                var scroll = $(window).scrollTop();
-                if (scroll >= 50) {
-                    $(".sticky").addClass("darkheader");
-                } else {
-                    $(".sticky").removeClass("darkheader");
-                }
-            });
-        },
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+            if (scroll >= 50) {
+                $(".sticky").addClass("darkheader");
+            } else {
+                $(".sticky").removeClass("darkheader");
+            }
+        });
+    },
 
         WebSite.prototype.initMagnificPopup = function () {
             $('.video-play-icon-trigger').magnificPopup({
@@ -88,35 +88,55 @@
             $('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
                 $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
             });
+            $(document).on('click touchend', function(e) {
+                var contactSuccess = $('#contact-success');
+                if (contactSuccess.css('display') !== "none")
+                {
+                    if (!$(e.target).is(contactSuccess))
+                    {
+                        e.preventDefault();
+                        contactSuccess.hide();
+                    }
+                }
+
+            });
             $('.contact-form form').submit(function(e) {
                 e.preventDefault();
                 $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
                 var postdata = $('.contact-form form').serialize();
+                $(this).find("button[type='submit']").text("Sending..");
                 $.ajax({
                     type: 'POST',
                     url: 'contact.php',
                     data: postdata,
-                    dataType: 'json',                    
+                    dataType: 'json',
                     success: function(json) {
+                        $(".contact-form form button[type='submit']").text("Send");
                         if(json.emailMessage != '') {
                             $('.contact-form form .contact-email').addClass('contact-error');
                         }
                         if(json.messageMessage != '') {
                             $('.contact-form form textarea').addClass('contact-error');
                         }
-                        if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
+
+
+                        if(json.emailMessage === '' && json.subjectMessage === '' && json.messageMessage === '') {
 
                             var frm  = $('.contact-form form');
-                            // var contactSuccess = $('contact-success');
-                            // createPopper(frm, contactSuccess), {
-                            //     placement: 'top',
-                            //   };
+                            var contactSuccess = $('#contact-success');
+                            contactSuccess.show();
+                            var popper = new Popper(frm, contactSuccess, {
+                                placement: 'bottom',
+                            });
                         }
+                    },
+                    error: function (request, status, error) {
+                        $(".contact-form form button[type='submit']").text("Send");
                     }
                 });
             });
         },
-    
+
 
         WebSite.prototype.init = function () {
             this.initNavbarStickey();
@@ -132,7 +152,7 @@
 }(window.jQuery),
 
 //initializing
-function ($) {
-    "use strict";
-    $.WebSite.init();
-}(window.jQuery);
+    function ($) {
+        "use strict";
+        $.WebSite.init();
+    }(window.jQuery);
